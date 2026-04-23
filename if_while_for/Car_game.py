cdf58eb8522ad69d6be75279bdf4facad217da03
lctruc24 <Chitruc0204@gmail.com>
2026-04-23 20:33:25 +0700

    Car-game

--- if_while_for/Car_game.py
+++ if_while_for/Car_game.py
@@ -1,26 +1,31 @@
-command = ""
 started = False
+
 while True:
     command = input("> ")
     if command.lower() == "start":
         if started:
-            print("Car is already started!")
+            print("Car already started...")
         else:
             started = True
-            print("Car started...")
+            print("Car started!")
+
     elif command.lower() == "stop":
         if not started:
-            print("Car is already stopped")
+            print("Car already stopped...")
         else:
             started = False
-            print("Car stopped.")
-    elif command == "help":
+            print("Car stopped!")
+    
+    elif command.lower() == "help" or command.lower() == "instruction":
         print("""
-            start - to start the car
-            stop - to stop the car
-            quit - to quit
+            'start' to start the car
+            'stop' to stop the car
+            'quit' to quit
             """)
+
     elif command.lower() == "quit":
         break
+
     else:
-        print("Sorry, I don't understand that!")
+        print("I don't understand your mean")
+
